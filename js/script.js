e9cd6e1530e7ec5d2ccf8855f80a6283bcd5f3c7
GauravWalia19 <gauravwalia019@gmail.com>
2018-12-12 11:52:36 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,108 @@
+(function(window, document, undefined)
+ {
+  var ENTER_KEY_CODE = 13;
+  var STATUS_OK = 200;
+
+  var taskTemplate = document.getElementById('task-template');
+  var renderTaskTemplate = Handlebars.compile(taskTemplate.innerHTML);
+
+  var taskInput = document.getElementById('task');
+  var taskList = document.getElementById('task-list');
+  var i=0;
+
+  var request = new XMLHttpRequest();
+  
+  request.addEventListener('load', function() 
+  {
+    if (request.status === STATUS_OK) {
+      var tasks = JSON.parse(request.responseText);
+
+      tasks.forEach(function(task) {
+        var li = document.createElement('li');
+        li.innerHTML = renderTaskTemplate({ task: task.text });
+		i++;
+		li.setAttribute("id",i);
+        taskList.appendChild(li);
+      });
+    }
+  });
+
+  request.open('GET', '/tasks');
+  request.send();
+
+  taskInput.addEventListener("keydown", function(event) 
+  {
+    if (event.keyCode === ENTER_KEY_CODE) 
+	{
+      event.preventDefault();
+      addTaskToList();
+    }
+  });
+
+  /* Adds a new task item to the list, with the value from the
+   * given input.
+   *
+   * Arguments:
+   * taskInput -- the HTMLElement input tag
+   */
+  function addTaskToList() 
+  {
+    if (taskInput.value) 
+	{
+      var request = new XMLHttpRequest();
+	  
+      request.addEventListener('load', function() 
+	  {
+        if (request.status === STATUS_OK) 
+		{
+          var li = document.createElement('li');
+          li.innerHTML = renderTaskTemplate({ task: taskInput.value });
+		  i++;
+		  li.setAttribute("id",i);
+          taskInput.value = "";
+          taskList.appendChild(li);
+        }
+      });
+      request.open('POST', '/tasks');
+      request.send(JSON.stringify({ text: taskInput.value }));
+    }
+  }
+
+  taskList.addEventListener('click', function(event) {
+    var target = event.target;
+    var li = target.parentNode;
+
+    if (target.classList.contains('check')) {
+      event.preventDefault();
+      if (li.classList.contains('checked')) {
+        li.classList.remove('checked');
+        target.innerHTML = '&#9744;';
+      } else {
+        li.classList.add('checked');
+        target.innerHTML = '&#9745;';
+      }
+
+      localStorage.tasks = taskList.innerHTML;
+    } else if (target.classList.contains("delete")) {
+      
+	 
+	  
+	  var request = new XMLHttpRequest();
+	  
+	  request.addEventListener('load', function() 
+	  {
+		if (request.status === STATUS_OK) {
+		  li.parentNode.removeChild(li);
+			localStorage.tasks = taskList.innerHTML;
+			
+		}
+	  });
+
+	  request.open('POST', '/deletetask');
+	  request.send(li.id);
+		  
+      
+    }
+  });
+
+})(this, this.document);
